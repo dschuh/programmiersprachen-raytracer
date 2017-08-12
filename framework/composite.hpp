e9160e6619d0b5a91268102c03d2e7f4c6898c02
David Schuh <david.schuh@uni-weimar.de>
2017-08-12 18:59:24 +0200

    Merged Succesfully

---
+++ framework/composite.hpp
@@ -0,0 +1,22 @@
+#ifndef COMPOSITE_HPP
+#define COMPOSITE_HPP
+
+#include "box.hpp"
+#include "sphere.hpp"
+#include "ray.hpp"
+
+class Composite : public Shape 
+{
+    Composite();
+    Composite(std::string const& name);
+    ~Composite();
+
+
+
+
+
+private:
+    std::string m_name;
+    std::shared_ptr<Shape>
+};
+#endif
--- framework/scene.hpp
+++ framework/scene.hpp
@@ -2,16 +2,16 @@
 #define SCENE_HPP
 
 #include "shape.hpp"
-/*
+
 #include "box.hpp"
 #include "sphere.hpp"
-<<<<<<< Updated upstream
 #include "material.hpp"
 #include "light.hpp"
 #include "camera.hpp"
 
 struct Scene{
 
+//Brauchen wir boxes uns spheres oder reichen shapes?
     Scene():
         lights{},
         ambiente{},
@@ -20,23 +20,14 @@ struct Scene{
         spheres{},
         boxes{}{}
         
-
     std::vector<Light> lights;
     Intensity ambiente;
     std::map<std::string, Material> materials;
     Camera camera;
+    std::shared_ptr<Shape>
     std::vector<Sphere> spheres;
     std::vector<Box> boxes;
-=======
-*/
-struct Scene{
 
-std::vector<shared_ptr<Shape>> shapes;
-std::map<Material> materials;
-std::vector<Light> lights;
-//Kamera kamera
->>>>>>> Stashed changes
-//usw.
 
 };
 
